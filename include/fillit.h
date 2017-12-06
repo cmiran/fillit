66cce3cb856b8b4408a6286708c45806e020a97f
cmiran <cmiran@student.42.fr>
2017-12-06 18:37:56 +0100

    rev fillit.h

--- include/fillit.h
+++ include/fillit.h
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 15:47:41 by cmiran            #+#    #+#             */
-/*   Updated: 2017/12/06 17:27:59 by cmiran           ###   ########.fr       */
+/*   Updated: 2017/12/06 17:41:48 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,25 @@
 # include <string.h>
 # include "libft.h"
 
-typedef struct	s_list
+typedef struct	s_map
 {
-	char		**tab;
-	struct s_list	*next;
-}		t_list;
+	int	size;
+	char	**tab;
+}		t_map;
+
+typedef struct	s_point
+{
+	int	x;
+	int	y;
+}		s_list;
+
+typedef struct	s_etris
+{
+	char	**pos;
+	int	width;
+	int	height;
+	char	value;
+
+}		t_etris;
 
 #endif
