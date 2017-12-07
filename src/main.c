fd3058bf0eb21025f3d08c6031da6f105d5d97e9
cmiran <cmiran@student.42.fr>
2017-12-07 18:00:57 +0100

    add main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pull_tetri.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/06 16:07:42 by cmiran            #+#    #+#             */
-/*   Updated: 2017/12/07 17:59:06 by cmiran           ###   ########.fr       */
+/*   Created: 2017/12/07 17:21:35 by cmiran            #+#    #+#             */
+/*   Updated: 2017/12/07 17:35:13 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fillit.h"
 #include "../include/libft.h"
 
-t_list	pull_tetri(char *fd)
+int	main(int argc, char **argv)
 {
-	return (list);	
+	t_list	*list;
+	
+	if (argc != 2 || !(list = pull_tetri(open(argv[1], O_RDONLY))))
+	{
+		ft_putstr("error\n");
+		return (EXIT_FAILURE);
+	}
+	return (0);
 }
