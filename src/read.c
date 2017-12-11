a513805ac19f478fd27a15c6d3fc94a212fcc622
cmiran <cmiran@student.42.fr>
2017-12-11 18:06:44 +0100

    del comments, debug stuff, rev read.c, linked list is reversed

--- src/read.c
+++ src/read.c
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 16:07:42 by cmiran            #+#    #+#             */
-/*   Updated: 2017/12/11 17:56:15 by cmiran           ###   ########.fr       */
+/*   Updated: 2017/12/11 18:05:20 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,8 @@ void	write_tetri(const char *str, t_etris *tetri, char id)
 	int	i;
 	int	x[4];
 	int	y[4];
-//	t_etris	*tetri;
 
 	i = 0;
-//	tetri = NULL;
 	get_pos(str, x, y);
 	while (i < 4)
 	{
@@ -56,7 +54,6 @@ void	write_tetri(const char *str, t_etris *tetri, char id)
 	}
 	tetri->id = id;
 	tetri->next = NULL;
-//	return (tetri);
 }
 
 int	check_adja(const char *str)
@@ -125,37 +122,18 @@ int	pull_list(const int fd, t_control *gofirst)
 	id = 'A';
 	while ((ret = read(fd, buf, 21)) >= 20)
 	{
-		printf("1.1\n");
 		if (!(check_chunk(buf, ret)))
 			return (0);
-//		if (!(tetri = ft_memalloc(sizeof(*tetri))))
-//		{
-//			printf("1.2\n");
-//			return (0);
-//		}
-		printf("1.3\n");
-		if (id == 'A')
+		if (id == 'A' && (tetri = ft_memalloc(sizeof(*tetri))))
 		{
-			printf("1.4\n");
-			if(!(tetri = ft_memalloc(sizeof(*tetri))))
-				return (0);
-//			tetri = write_tetri(buf, id++);
 			write_tetri(buf, tetri, id++);
-			printf("1.5\n");
 			gofirst->first = tetri;
-			printf("1.6\n");
 		}	
-		else
+		else if ((new = ft_memalloc(sizeof(*new))))
 		{
-			printf("1.7\n");
-			if(!(new = ft_memalloc(sizeof(*tetri))))
-				return (0);
 			write_tetri(buf, new, id++);
 			new->next = gofirst->first;
 			gofirst->first = new;
-			printf("1.8\n");
-//			if(!(tetri = ft_memalloc(sizeof(*tetri))))
-//			return (0);
 		}
 	}
 	gofirst->i = id - 'A';
