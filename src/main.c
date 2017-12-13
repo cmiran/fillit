5272bade6e13b4bddb350b817d7a3973823a62ef
cmiran <cmiran@student.42.fr>
2017-12-13 02:37:35 +0100

    rev print_list, nothing fancy.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 17:21:35 by cmiran            #+#    #+#             */
-/*   Updated: 2017/12/11 18:07:46 by cmiran           ###   ########.fr       */
+/*   Updated: 2017/12/13 02:36:50 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,25 @@ void	print_list(t_control *gofirst)
 {
 	t_etris	*current;
 	int			i;
-
-	printf("nbr tetri : %d\n", gofirst->i);
+	
+	printf("#########################\n");
+	printf("#  nbr tetriminos : %d  #\n", gofirst->i);
+	printf("#########################\n");
 	current = gofirst->first;
 	while (current != NULL)
 	{
 		i = 0;
-		printf("-\n");
-		printf("id	%c\n", current->id);
-		printf("X 	");
+		printf("#########################\n");
+		printf("#  id ->  %c		#\n", current->id);
+		printf("#   X ->  ");
 		while (i < 4)
 			printf("%d  ", current->x[i++]);
 		i = 0;
-		printf("\nY 	");
+		printf("	#\n#   Y ->  ");
 		while (i < 4)
 			printf("%d  ", current->y[i++]);
-		printf("\n");
+		printf("	#\n");
+		printf("#########################\n");
 		current = current->next;
 	}
 }
